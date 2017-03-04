d42ccc54d0f721650709f268ae8c414fba7543bc
natrok <diegorok3@yahoo.es>
2017-03-05 00:05:52 +0100

    modification for calculating log, i tested and was ok but is neccesary an order

--- color-transfer.c
+++ color-transfer.c
@@ -51,24 +51,32 @@ static float LMS2RGB[D][D] = {
   {0.0497, -0.2439, 1.2045}
 };
 
+/**
+ * Calcule of a mean value
+ * @param <vec> the vector where we can calculate the mean
+ * @param <means> vector where we stock the means
+ * @param pxls: size of the first vector
+ * */
 static void mean(float* vec, float* means, int pxls)
 {
-  printf("%f %f %f \n", means[0], means[1], means[2]);
   for(int i = 0; i < pxls; i++)
   {
     means[0] += vec[i*3];
 	means[1] += vec[i*3+1];
 	means[2] += vec[i*3+2];
-	//if(i > pxls/4+3600 && i < pxls/4 + 4600)
-	//printf("%d %f %f %f \n", i, means[0], vec[i*3], means[2]);
   }
-  printf("%f %f %f \n", means[0], means[1], means[2]);
-	  
   means[0] = means[0]/pxls;
   means[1] = means[1]/pxls;
   means[2] = means[2]/pxls;
 }
 
+/**
+ * Standard desviation
+ * @param <vec> the vector where we can calculate the mean
+ * @param <sds> standard desviation of source
+ * @param <means> means of source
+ * @param pxls: size of the first vector
+ * */
 static void sd(float* vec, float* sds, float* means, int pxls)
 {
   for(int i = 0; i < pxls; i++)
@@ -90,22 +98,22 @@ static void multiply(float* vec, float matr[D][D])
   vec[2] = matr[2][0]*x1 + matr[2][1]*x2 + matr[2][2]*x3;
 }
 
-static void transform(float* img, float matr[D][D], int pxls)
+static void transform(float *img, float matr[D][D], int pxls)
 {
-  float* addr = img;
+  //float *addr = img;
   for(int i = 0; i < pxls; i++)
   {
     multiply(img, matr);
     img+=3;
   }
-  img = addr;
+  //img = addr; //why???
 }
 
-static void toLogSpace(float* img, int pxls)
+static void toLogSpace(float *img, int pxls)
 {
   for(int i = 0; i < pxls*3; i++)
   {
-    img[i] = log10(img[i]+1); //add 1 to avoid log(0)=inf
+   img[i]= (img[i]==0.0f) ? 0:log10(img[i]);//avoid log(0)=inf   
   }
 }
 
@@ -113,65 +121,62 @@ static void toLinSpace(float* img, int pxls)
 {
   for(int i = 0; i < pxls*3; i++)
   {
-    img[i] = pow(10, img[i])-1; //substract the previously added 1
+    img[i] = pow(10, img[i]);
   }
 }
 
 
-
 static void process(char *ims, char *imt, char* imd){
+ 
+  //first image attributes
   pnm img = pnm_load(ims);  
   int rows = pnm_get_height(img);
   int cols = pnm_get_width(img);
   
+  //second image attributes  
   pnm img2 = pnm_load(imt);  
   int rows2 = pnm_get_height(img2);
   int cols2 = pnm_get_width(img2);
-
-	
+  
   pnm newi = pnm_new(cols2, rows2, PnmRawPpm);
 
   unsigned short *imgSource = pnm_get_image(img);
   unsigned short *imgTarget = pnm_get_image(img2);
   unsigned short *result = pnm_get_image(newi);
+  
   float *source = (float*)malloc(sizeof(float)*3*rows*cols);
   float *target = (float*)malloc(sizeof(float)*3*rows2*cols2);
-  printf("a: %d %d %d \n",  imgSource[70071*3 + 0], imgSource[70071*3 + 1], imgSource[70071*3 + 2]);
+ 
   for(int i = 0; i < rows*cols*3; i++)
-  {
 	source[i] = (float)imgSource[i];
-  }
+  
   for(int i = 0; i < rows2*cols2*3; i++)
-  {
-	target[i] = (float)imgTarget[i];
-  }
+  	target[i] = (float)imgTarget[i];
+  
 
   //------transform to lab space------//
   transform(source, RGB2LMS, cols*rows);
   transform(target, RGB2LMS, cols2*rows2);
-  printf("a: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  
   toLogSpace(source, cols*rows);
   toLogSpace(target, cols2*rows2);
-  printf("log: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  
   transform(source, LMS2lab1, cols*rows);
   transform(target, LMS2lab1, cols2*rows2);
-  printf("a: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  
   transform(source, LMS2lab2, cols*rows);
   transform(target, LMS2lab2, cols2*rows2);
   
-  printf("a: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
-  
   //------color transfer------//
-  
-  float meanS[3] = {0};
-  float meanT[3] = {0};
+  float meanSource[3] = {0};
+  float meanTarget[3] = {0};
   float sdS[3] = {0};
   float sdT[3] = {0};
   float sdNorm[3] = {0};
-  mean(source, meanS, rows*cols);
-  mean(target, meanT, rows2*cols2);
-  sd(source, sdS, meanS, rows*cols);
-  sd(target, sdT, meanT, rows2*cols2);
+  mean(source, meanSource, rows*cols);
+  mean(target, meanTarget, rows2*cols2);
+  sd(source, sdS, meanSource, rows*cols);
+  sd(target, sdT, meanTarget, rows2*cols2);
   for(int c = 0; c < 3; c++)
   {
 	  sdNorm[c] = sdT[c]/sdS[c]; 
@@ -186,22 +191,16 @@ static void process(char *ims, char *imt, char* imd){
   //------transform to RGB space------//
   transform(source, lab2LMS1, cols*rows);
   transform(target, lab2LMS1, cols2*rows2);
+  
   transform(source, lab2LMS2, cols*rows);
   transform(target, lab2LMS2, cols2*rows2);
+  
   toLinSpace(source, cols*rows);
   toLinSpace(target, cols2*rows2);
+  
   transform(source, LMS2RGB, cols*rows);
   transform(target, LMS2RGB, cols2*rows2);
 
-  /*float* a;
-  a = (float*)malloc(6*sizeof(float));
-  for(int i = 0; i < 6; i++)
-   a[i]=1.0;
-  transform(a, lab2LMS2, 2);
-  transform(a, lab2LMS2, 2);*/
-  //for(int i = 0; i < rows*cols; i++)
-  //printf("%d, %d, %d, %f, %f, %f\n", imgSource[i*3], imgSource[i*3+1], imgSource[i*3+2], source[i*3], source[i*3+1], source[i*3+2]);
-  
   for(int i = 0; i < rows2*cols2*3; i++)
   {
 	result[i] = roundf(target[i]);
@@ -224,15 +223,8 @@ void usage (char *s){
 #define param 3
 int main(int argc, char *argv[]){
 
-
-
-
-
   if (argc != param+1) 
-    usage(argv[0]);
+  usage(argv[0]);
   process(argv[1], argv[2], argv[3]);
   return EXIT_SUCCESS;
-
-
-
 }
