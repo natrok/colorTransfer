896de75b6947176f30e6775908104772b6cc36af
Augustin Wilberg <augustinwilberg@gmx.de>
2017-03-04 19:05:12 +0100

    added code... result not satisfying yet

--- color-transfer.c
+++ color-transfer.c
@@ -51,24 +51,35 @@ static float LMS2RGB[D][D] = {
   {0.0497, -0.2439, 1.2045}
 };
 
-static float mean(float* vec, int pxls)
+static void mean(float* vec, float* means, int pxls)
 {
-  float sum = 0.0;
+  printf("%f %f %f \n", means[0], means[1], means[2]);
   for(int i = 0; i < pxls; i++)
   {
-    sum += vec[i];
+    means[0] += vec[i*3];
+	means[1] += vec[i*3+1];
+	means[2] += vec[i*3+2];
+	//if(i > pxls/4+3600 && i < pxls/4 + 4600)
+	//printf("%d %f %f %f \n", i, means[0], vec[i*3], means[2]);
   }
-  return sum/pxls;
+  printf("%f %f %f \n", means[0], means[1], means[2]);
+	  
+  means[0] = means[0]/pxls;
+  means[1] = means[1]/pxls;
+  means[2] = means[2]/pxls;
 }
 
-static float sd(float* vec, int pxls, float mean)
+static void sd(float* vec, float* sds, float* means, int pxls)
 {
-  float sum = 0.0;
   for(int i = 0; i < pxls; i++)
   {
-    sum += pow(vec[i] - mean, 2);
+	sds[0] += pow(vec[i*3] - means[0], 2);
+	sds[1] += pow(vec[i*3+1] - means[1], 2);
+	sds[2] += pow(vec[i*3+2] - means[2], 2);
   }
-  return sqrt(sum/(pxls-1));
+  sds[0] = sqrt(sds[0]/(pxls-1));
+  sds[1] = sqrt(sds[1]/(pxls-1));
+  sds[2] = sqrt(sds[2]/(pxls-1));
 }
 
 static void multiply(float* vec, float matr[D][D])
@@ -90,6 +101,22 @@ static void transform(float* img, float matr[D][D], int pxls)
   img = addr;
 }
 
+static void toLogSpace(float* img, int pxls)
+{
+  for(int i = 0; i < pxls*3; i++)
+  {
+    img[i] = log10(img[i]+1); //add 1 to avoid log(0)=inf
+  }
+}
+
+static void toLinSpace(float* img, int pxls)
+{
+  for(int i = 0; i < pxls*3; i++)
+  {
+    img[i] = pow(10, img[i])-1; //substract the previously added 1
+  }
+}
+
 
 
 static void process(char *ims, char *imt, char* imd){
@@ -98,21 +125,73 @@ static void process(char *ims, char *imt, char* imd){
   int cols = pnm_get_width(img);
   
   pnm img2 = pnm_load(imt);  
-  int rows2 = pnm_get_height(imt);
-  int cols2 = pnm_get_width(imt);
+  int rows2 = pnm_get_height(img2);
+  int cols2 = pnm_get_width(img2);
 
 	
   pnm newi = pnm_new(cols2, rows2, PnmRawPpm);
 
   unsigned short *imgSource = pnm_get_image(img);
   unsigned short *imgTarget = pnm_get_image(img2);
+  unsigned short *result = pnm_get_image(newi);
+  float *source = (float*)malloc(sizeof(float)*3*rows*cols);
+  float *target = (float*)malloc(sizeof(float)*3*rows2*cols2);
+  printf("a: %d %d %d \n",  imgSource[70071*3 + 0], imgSource[70071*3 + 1], imgSource[70071*3 + 2]);
+  for(int i = 0; i < rows*cols*3; i++)
+  {
+	source[i] = (float)imgSource[i];
+  }
+  for(int i = 0; i < rows2*cols2*3; i++)
+  {
+	target[i] = (float)imgTarget[i];
+  }
 
-  for(int i = 0; i < rows2; i++)
-		{
-			for(int j = 0; j < cols2; j++)
-			{}
-	         }
-
+  //------transform to lab space------//
+  transform(source, RGB2LMS, cols*rows);
+  transform(target, RGB2LMS, cols2*rows2);
+  printf("a: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  toLogSpace(source, cols*rows);
+  toLogSpace(target, cols2*rows2);
+  printf("log: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  transform(source, LMS2lab1, cols*rows);
+  transform(target, LMS2lab1, cols2*rows2);
+  printf("a: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  transform(source, LMS2lab2, cols*rows);
+  transform(target, LMS2lab2, cols2*rows2);
+  
+  printf("a: %f %f %f \n",  source[70071*3 + 0], source[70071*3 + 1], source[70071*3 + 2]);
+  
+  //------color transfer------//
+  
+  float meanS[3] = {0};
+  float meanT[3] = {0};
+  float sdS[3] = {0};
+  float sdT[3] = {0};
+  float sdNorm[3] = {0};
+  mean(source, meanS, rows*cols);
+  mean(target, meanT, rows2*cols2);
+  sd(source, sdS, meanS, rows*cols);
+  sd(target, sdT, meanT, rows2*cols2);
+  for(int c = 0; c < 3; c++)
+  {
+	  sdNorm[c] = sdT[c]/sdS[c]; 
+	  //printf("%f %f %f \n", sdNorm[c], meanT[c], meanS[c]);
+	  for(int i = 0; i < cols2*rows2; i++)
+	  {
+		target[i*3+c] = sdNorm[c]*(target[i*3+c] - meanT[c]) + meanS[c];
+	  }
+  }
+  
+  
+  //------transform to RGB space------//
+  transform(source, lab2LMS1, cols*rows);
+  transform(target, lab2LMS1, cols2*rows2);
+  transform(source, lab2LMS2, cols*rows);
+  transform(target, lab2LMS2, cols2*rows2);
+  toLinSpace(source, cols*rows);
+  toLinSpace(target, cols2*rows2);
+  transform(source, LMS2RGB, cols*rows);
+  transform(target, LMS2RGB, cols2*rows2);
 
   /*float* a;
   a = (float*)malloc(6*sizeof(float));
@@ -120,10 +199,20 @@ static void process(char *ims, char *imt, char* imd){
    a[i]=1.0;
   transform(a, lab2LMS2, 2);
   transform(a, lab2LMS2, 2);*/
-  (void) ims;
-  (void) imt;
-  (void) imd;
-  //printf("%f, %f, %f, %f, %f, %f", a[0], a[1], a[2], a[3], a[4], a[5]);
+  //for(int i = 0; i < rows*cols; i++)
+  //printf("%d, %d, %d, %f, %f, %f\n", imgSource[i*3], imgSource[i*3+1], imgSource[i*3+2], source[i*3], source[i*3+1], source[i*3+2]);
+  
+  for(int i = 0; i < rows2*cols2*3; i++)
+  {
+	result[i] = roundf(target[i]);
+  }
+  
+  pnm_save(newi, PnmRawPpm, imd);  
+  pnm_free(img);
+  pnm_free(img2);
+  pnm_free(newi);
+  free(source);
+  free(target);
 }
 
 void usage (char *s){
